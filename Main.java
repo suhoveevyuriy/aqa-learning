940dbe8dfcb03ff22b285bec86429d7a02874633
suhoveevyuriy <suhoveev.yuriy@gmail.com>
2026-08-24 23:16:49 +0200

    Change words in Student Report

--- Main.java
+++ Main.java
@@ -30,9 +30,9 @@ public class Main {
         }
 
         System.out.println("\n===== Student Report =====");
-        System.out.println("Name: " + student.getName());
-        System.out.println("Grade: " + student.getGrade());
-        System.out.println("Level: " + student.getLevel());
+        System.out.println("Student's Name: " + student.getName());
+        System.out.println("Student's Grade: " + student.getGrade());
+        System.out.println("Student's Level: " + student.getLevel());
 
     }
 }
