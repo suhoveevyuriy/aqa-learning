678b67e13d05dee09158d171e9a1e0e8c16fcd87
suhoveevyuriy <suhoveev.yuriy@gmail.com>
2026-08-24 22:36:39 +0200

    Add text about steps to input massage

--- Main.java
+++ Main.java
@@ -9,14 +9,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("========= Student Grades Validator ==========");
 
-        System.out.print("Enter student name: ");
+        System.out.print("First step.Enter student name: ");
         String name = scanner.nextLine();
 
         Student student;
 
         while (true) {
 
-            System.out.print("Enter grade (1-12): ");
+            System.out.print("Second step.Enter grade (1-12): ");
 
             int grade = scanner.nextInt();
 
